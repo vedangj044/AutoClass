130000f9b1f440b30c8177376f15f4e6d58b7e6c
vedangj044 <vedangj044@gmail.com>
2021-10-20 14:48:08 +0530

    api

--- app.py
+++ app.py
@@ -2,16 +2,29 @@ from enum import unique
 from fastapi import FastAPI, HTTPException, status, Depends
 from fastapi.params import Body
 import pymongo
+import random
+import string
 import os
-
 from pymongo import message
-from pymongo import database
+# from passlib.hash import bcrypt
+import json
+import pymongo.database as mdb
+from pymongo.errors import DuplicateKeyError
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+JWT_SECRET = os.environ['JWT']
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl='token')
+
+class MessageModel(BaseModel):
+    message: str
 
 databse_url = "mongodb://root:rootpassword@localhost:27017/admin"
 
 def get_db():
     engine = pymongo.MongoClient(databse_url)
-    db_name = "collect"
+    db_name = "atuoclass"
 
     try:
         db = engine.get_database(db_name)
@@ -20,5 +33,44 @@ def get_db():
     finally:
         engine.close()
 
+# def authenticate_user(username: str, password: str, db):
+#     user = db['users'].find_one({"username": username})
+#     if not user:
+#         return False
+#     if not bcrypt.verify(password, user["password"]):
+#         return False
+
+#     return json.loads(dumps(user))
+
 app = FastAPI(title="AutoClass", description="Youtube classroom")
 
+def get_class_name():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+
+@app.get("/create_class")
+def create_class(db: mdb.Database = Depends(get_db)):
+
+    class_name = get_class_name()
+    db["classes"].insert_one({"name": class_name})
+    db.create_collection(class_name)
+
+    return MessageModel(message = "Class created successfully - " + class_name)
+
+@app.get("/class/{class_code}")
+def join_class(class_code, db: mdb.Database = Depends(get_db)):
+    
+    l = db["classes"].find_one({"name": class_code})
+    if l is None:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Class not found")
+    
+    return MessageModel(message = "Joined class successfully.")
+
+@app.post("/class/{class_code}")
+def post_assignment(class_code, db: mdb.Database = Depends(get_db)):
+
+    l = db["classes"].find_one({"name": class_code})
+    if l is None:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Class not found")
+    
+    url = ""
+    return MessageModel(message = "Added assignment")
--- question_gen.py
+++ question_gen.py
@@ -1,5 +1,10 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import json
+from rake_nltk import Rake
+
+import nltk
+nltk.download("stopwords")
+nltk.download("punkt")
 
 transprint = YouTubeTranscriptApi.get_transcript("F1F2imiOJfk")
 l = ""
@@ -8,5 +13,13 @@ for i in transprint:
     l += i["text"] + " "
 
 
-with open("trans.json", "w") as f:
-    f.write(l)
+# with open("trans.json", "w") as f:
+#     f.write(l)
+
+def get_topics(l):
+    r = Rake()
+    r.extract_keywords_from_text(l)
+
+    return r.get_ranked_phrases()
+
+print(get_topics(l))
